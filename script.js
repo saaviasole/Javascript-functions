7d2f4723e217b1c0a97670f993ee6f90e2cd2050
saaviasole <swapnaliasole@yahoo.com>
2025-10-17 16:59:19 +0530

    function

---
+++ script.js
@@ -0,0 +1,33 @@
+function greet() {
+    console.log("Nice to meet you");
+}
+
+greet()
+
+var greetMessage = function() {
+    console.log("h1 Good day")
+};
+
+greetMessege()
+
+
+function add(a, b) {
+    console.log(a + b)
+}
+
+
+add(1, 2)
+
+
+function substract(a, b) {
+    return a - b
+}
+var result = substract(5, 2)
+console.log(result)
+
+
+function students() {
+    var a = 2
+}
+
+console.log(a)
